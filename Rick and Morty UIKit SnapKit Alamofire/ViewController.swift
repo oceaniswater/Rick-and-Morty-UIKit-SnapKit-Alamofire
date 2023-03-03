216dcfe21962228e008a40bfdd2797155e35ea76
markgolubev <markflyingbear@gmail.com>
2023-03-03 11:57:08 +0000

    Initial Commit

---
+++ Rick and Morty UIKit SnapKit Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty UIKit SnapKit Alamofire
+//
+//  Created by Марк Голубев on 03.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
